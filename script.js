d73da569d09d845b53656268fb7a83c43866ae90
kcgr0690 <kcgr0690@gmail.com>
2026-02-13 18:26:08 -0500

    Started js for the balance, starting a deposit function.

---
+++ script.js
@@ -0,0 +1,19 @@
+//total balance javascript here. will probably comment for every feature to make it easier for myself later.
+
+const balanceEl = document.getElementById('balance');
+let balance = localStorage.getItem('balance');
+
+if (balance === null) {
+    balance = 0;
+} else {
+    balance = parseFloat(balance);
+    // ^^ converting saved string from past website use to a float so I can access it here
+}
+
+balanceEl.textContent = `$${balance.tofixed(2)}`;
+
+//making a function to deposit an amount.
+
+function deposit(amount) {
+    
+}
